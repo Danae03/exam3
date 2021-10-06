450fedba1d15527c379afdbc54bfce3f6f48a10c
Your Name <you@example.com>
2021-10-05 23:08:52 -0500

    hola

---
+++ app.js
@@ -0,0 +1,53 @@
+const btn1 = document.getElementById("btn1")
+const audio1 = document.getElementById("audio1")
+const btn2 = document.getElementById("btn2")
+const audio2 = document.getElementById("audio2")
+const btn3 = document.getElementById("btn3")
+const audio3 = document.getElementById("audio3")
+const btn4 = document.getElementById("btn4")
+const audio4 = document.getElementById("audio4")
+const btn5 = document.getElementById("btn5")
+const audio5 = document.getElementById("audio5")
+const btn6 = document.getElementById("btn6")
+const audio6 = document.getElementById("audio6")
+const btn7 = document.getElementById("btn7")
+const audio7 = document.getElementById("audio7")
+const btn8 = document.getElementById("btn8")
+const audio8 = document.getElementById("audio8")
+const btn9 = document.getElementById("btn9")
+const audio9 = document.getElementById("audio9")
+btn1.addEventListener('click', ()=>{
+    audio1.play();
+})
+
+btn2.addEventListener('click', ()=>{
+    audio2.play();
+})
+
+btn3.addEventListener('click', ()=>{
+    audio3.play();
+})
+
+btn4.addEventListener('click', ()=>{
+    audio4.play();
+})
+
+btn5.addEventListener('click', ()=>{
+    audio5.play();
+})
+
+btn6.addEventListener('click', ()=>{
+    audio6.play();
+})
+
+btn7.addEventListener('click', ()=>{
+    audio7.play();
+})
+
+btn8.addEventListener('click', ()=>{
+    audio8.play();
+})
+
+btn9.addEventListener('click', ()=>{
+    audio9.play();
+})
